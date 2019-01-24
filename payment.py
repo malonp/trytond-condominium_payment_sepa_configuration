60951752b16c80837dd946f2460830c35d4d3b0f
map <port02.server@gmail.com>
2019-01-24 20:06:08 +0000

    update version

--- payment.py
+++ payment.py
@@ -72,18 +72,17 @@ class CondoPaymentGroup(metaclass=PoolMeta):
 
         if id:
             Company = Pool().get('company.company')
-            condo = Company.search([('id', '=', id),
-                                    ('is_Condominium', '=', True),
-                                    ('sepa_creditor_identifier', '!=', None),
-                                    ('party.active', '=', True),
-                                  ])
+            condominium = Company(id)
 
-            if (len(condo)==1 and len(condo[0].sepa_mandates)>0):
-                bankaccountnumber = [number for account in condo[0].party.bank_accounts if account.active for number in account.numbers if number.type=='iban' ]
+            if condominium and condominium.is_Condominium and \
+                condominium.sepa_creditor_identifier is not None and \
+                condominium.party.active and len(condominium.sepa_mandates)>0:
 
-                if (len(bankaccountnumber)==1 or condo[0].company_account_number) and\
-                    condo[0].company_sepa_batch_booking is not None and\
-                    condo[0].company_sepa_charge_bearer is not None:
+                bankaccountnumber = [number for account in condominium.party.bank_accounts if account.active for number in account.numbers if number.type=='iban']
+
+                if (len(bankaccountnumber)==1 or condominium.company_account_number) and\
+                    condominium.company_sepa_batch_booking is not None and\
+                    condominium.company_sepa_charge_bearer is not None:
                     if 'dates' in kwargs:
                         #get biggest date
                         date = sorted([d[2] for d in filter(lambda x:not x[1], kwargs['dates'])], reverse=True)[0]
@@ -92,19 +91,24 @@ class CondoPaymentGroup(metaclass=PoolMeta):
                         date_arg = date.date()
                         reference = '{:04d}'.format(date_arg.year) + '_' + \
                                     '{:02d}'.format(date_arg.month) + '-' + \
-                                    (condo[0].company_account_number or bankaccountnumber[0]).number_compact [8:12] + '.' + \
+                                    (condominium.company_account_number or bankaccountnumber[0]).number_compact [8:12] + '.' + \
                                     '{:04d}'.format(date_arg.year)[-2:]
 
-                        payments = cls.search([('company', '=', condo[0]),
+                        payments = cls.search([('company', '=', condominium),
                                                ('reference', '=', reference),])
                         if not len(payments):
                             paymentgroup = cls(reference = reference,
-                                               company = condo[0],
-                                               account_number = condo[0].company_account_number or bankaccountnumber[0],
+                                               company = condominium,
+                                               account_number = condominium.company_account_number or bankaccountnumber[0],
                                                date = date_arg,
-                                               sepa_batch_booking = condo[0].company_sepa_batch_booking,
-                                               sepa_charge_bearer = condo[0].company_sepa_charge_bearer,
+                                               sepa_batch_booking = condominium.company_sepa_batch_booking,
+                                               sepa_charge_bearer = condominium.company_sepa_charge_bearer,
                                                )
                             paymentgroup.save()
                         else:
-                            logger.error('Unable to create condo payment group with reference: %s for %s', reference, condo[0].party.name)
+                            logger.error('Unable to create condo payment group with reference: %s for %s', reference, condominium.party.name)
+            else:
+                if condominium:
+                    logger.error('Company %s dont check conditions to prepare payment group', condominium.party.name)
+                else:
+                    logger.error('Unable to prepare payment group. Cant find company with id: %s', id)

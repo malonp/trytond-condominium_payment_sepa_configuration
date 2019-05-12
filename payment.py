bd9a033e345530b1185d9fdc937346b9064d36db
map <port02.server@gmail.com>
2019-05-12 21:56:13 +0100

    update version

--- payment.py
+++ payment.py
@@ -26,7 +26,7 @@ from trytond.transaction import Transaction
 import datetime
 import logging
 
-__all__ = ['CondoPain', 'CondoPaymentGroup']
+__all__ = ['CondoPain', 'Group']
 
 
 logger = logging.getLogger(__name__)
@@ -40,12 +40,13 @@ class CondoPain(metaclass=PoolMeta):
         super(CondoPain, cls).__setup__()
         cls._order.insert(0, ('reference', 'DESC'))
 
-class CondoPaymentGroup(metaclass=PoolMeta):
+
+class Group(metaclass=PoolMeta):
     __name__ = 'condo.payment.group'
 
     @classmethod
     def __setup__(cls):
-        super(CondoPaymentGroup, cls).__setup__()
+        super(Group, cls).__setup__()
         cls._order.insert(0, ('date', 'DESC'))
         cls._order.insert(1, ('reference', 'DESC'))
 
@@ -54,17 +55,17 @@ class CondoPaymentGroup(metaclass=PoolMeta):
         pool = Pool()
         Date = pool.get('ir.date')
         d = Date.today()
-        #set tomorrow (or the next business day after tomorrow) as date
-        next = d + datetime.timedelta(days= 7-d.weekday() if d.weekday()>3 else 1)
+        # set tomorrow (or the next business day after tomorrow) as date
+        next = d + datetime.timedelta(days=7 - d.weekday() if d.weekday() > 3 else 1)
         return next
 
     @classmethod
     def search_readonly(cls, name, domain):
         user = Transaction().user
-        if user<=1:
+        if user <= 1:
             return []
 
-        super(CondoPaymentGroup, cls).search_readonly(name, domain)
+        super(Group, cls).search_readonly(name, domain)
 
     @classmethod
     def PreparePaymentGroup(cls, **kwargs):
@@ -72,39 +73,64 @@ class CondoPaymentGroup(metaclass=PoolMeta):
 
         if id:
             Company = Pool().get('company.company')
-            condo = Company.search([('id', '=', id),
-                                    ('is_Condominium', '=', True),
-                                    ('sepa_creditor_identifier', '!=', None),
-                                    ('party.active', '=', True),
-                                  ])
+            condominium = Company(id)
 
-            if (len(condo)==1 and len(condo[0].sepa_mandates)>0):
-                bankaccountnumber = [number for account in condo[0].party.bank_accounts if account.active for number in account.numbers if number.type=='iban' ]
+            if (
+                condominium
+                and condominium.is_condo
+                and condominium.sepa_creditor_identifier is not None
+                and condominium.party.active
+                and len(condominium.mandates) > 0
+            ):
 
-                if (len(bankaccountnumber)==1 or condo[0].company_account_number) and\
-                    condo[0].company_sepa_batch_booking is not None and\
-                    condo[0].company_sepa_charge_bearer is not None:
+                bankaccountnumber = [
+                    number
+                    for account in condominium.party.bank_accounts
+                    if account.active
+                    for number in account.numbers
+                    if number.type == 'iban'
+                ]
+
+                if (
+                    (len(bankaccountnumber) == 1 or condominium.company_account_number)
+                    and condominium.company_sepa_batch_booking is not None
+                    and condominium.company_sepa_charge_bearer is not None
+                ):
                     if 'dates' in kwargs:
-                        #get biggest date
-                        date = sorted([d[2] for d in filter(lambda x:not x[1], kwargs['dates'])], reverse=True)[0]
+                        # get biggest date
+                        date = sorted([d[2] for d in filter(lambda x: not x[1], kwargs['dates'])], reverse=True)[0]
 
                     if date:
                         date_arg = date.date()
-                        reference = '{:04d}'.format(date_arg.year) + '_' + \
-                                    '{:02d}'.format(date_arg.month) + '-' + \
-                                    (condo[0].company_account_number or bankaccountnumber[0]).number_compact [8:12] + '.' + \
-                                    '{:04d}'.format(date_arg.year)[-2:]
+                        reference = (
+                            '{:04d}'.format(date_arg.year)
+                            + '_'
+                            + '{:02d}'.format(date_arg.month)
+                            + '-'
+                            + (condominium.company_account_number or bankaccountnumber[0]).number_compact[8:12]
+                            + '.'
+                            + '{:04d}'.format(date_arg.year)[-2:]
+                        )
 
-                        payments = cls.search([('company', '=', condo[0]),
-                                               ('reference', '=', reference),])
+                        payments = cls.search([('company', '=', condominium), ('reference', '=', reference)])
                         if not len(payments):
-                            paymentgroup = cls(reference = reference,
-                                               company = condo[0],
-                                               account_number = condo[0].company_account_number or bankaccountnumber[0],
-                                               date = date_arg,
-                                               sepa_batch_booking = condo[0].company_sepa_batch_booking,
-                                               sepa_charge_bearer = condo[0].company_sepa_charge_bearer,
-                                               )
+                            paymentgroup = cls(
+                                reference=reference,
+                                company=condominium,
+                                account_number=condominium.company_account_number or bankaccountnumber[0],
+                                date=date_arg,
+                                sepa_batch_booking=condominium.company_sepa_batch_booking,
+                                sepa_charge_bearer=condominium.company_sepa_charge_bearer,
+                            )
                             paymentgroup.save()
                         else:
-                            logger.error('Unable to create condo payment group with reference: %s for %s', reference, condo[0].party.name)
+                            logger.error(
+                                'Unable to create condo payment group with reference: %s for %s',
+                                reference,
+                                condominium.party.name,
+                            )
+            else:
+                if condominium:
+                    logger.error('Company %s dont check conditions to prepare payment group', condominium.party.name)
+                else:
+                    logger.error('Unable to prepare payment group. Cant find company with id: %s', id)
